185912439f852f6c93898c69caa6795044235594
Lourenço Basso <l.oliveirabasso@gmail.com>
2020-08-04 21:06:32 -0300

    Adicionados modificadores de acesso e métodos acessores

--- src/entities/Triangle.java
+++ src/entities/Triangle.java
@@ -1,10 +1,28 @@
 package entities;
 public class Triangle {
-	public double a;
-	public double b;
-	public double c;
+	private double a;
+	private double b;
+	private double c;
 	public double area() {
 		double p = (a + b + c) / 2.0;
 		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+	}
+	public double getA() {
+		return a;
+	}
+	public void setA(double a) {
+		this.a = a;
+	}
+	public double getB() {
+		return b;
+	}
+	public void setB(double b) {
+		this.b = b;
+	}
+	public double getC() {
+		return c;
+	}
+	public void setC(double c) {
+		this.c = c;
 	}
 }

5bdec710e38082bfe15e40b94658d7a62f96dc34
Lourenço Basso <l.oliveirabasso@gmail.com>
2020-08-05 08:53:14 -0300

    Adicionado construtor à classe Conta

--- src/entities/Triangle.java
+++ src/entities/Triangle.java
@@ -3,6 +3,11 @@ public class Triangle {
 	private double a;
 	private double b;
 	private double c;
+	public Triangle(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
 	public double area() {
 		double p = (a + b + c) / 2.0;
 		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
